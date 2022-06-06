683f12464df244377b4732e26ec01b1bc2456b4c
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-06-06 00:15:36 -0700

    added psuedocode

---
+++ assets/logic.js
@@ -0,0 +1,20 @@
+//moment js feature for current day
+//moment().format("dddd MMMM Do")
+
+//capture variabes for user input
+
+//function to determine whether or not time has already passed
+//color code in respons
+// if statements
+// if (timebox < moment)
+// if (timebox > moment)
+// if (timebox == moment)
+
+//moment function
+//capture moment for current time and 
+//does this onload in tandem with color code function
+
+
+
+//event listener to save to local storage
+//key for each hour
